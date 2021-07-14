ec3ea5aae2b6ac9f3434a4e95697c4347a6ff015
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-07-14 13:20:18 -0700

    add function to loop through an array of tweets and render each, the function also clears article area to prevent double posting

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,21 +4,43 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-};
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
 $(document).ready(() => {
-  const createTweetElement = (tweetData) => {
-    const user = tweetData.user;
+
+  const renderTweets = (tweets) => {
+    $('#tweet-container').empty();
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweet-container').prepend($tweet);
+    }
+  };
+
+  const createTweetElement = (tweet) => {
+    const user = tweet.user;
     const $tweet = $('<article>');
     const $header = $('<header>');
     const $avatar = $(`<img src='${user.avatars}'>`);
@@ -27,19 +49,18 @@ $(document).ready(() => {
     const $userid = $('<h3>').addClass('username').text(`${user.handle}`);
     $header.append($name, $userid);
     
-    const $content = $('<h3>').addClass('tweet').text(`${tweetData.content.text}`);
+    const $content = $('<h3>').addClass('tweet').text(`${tweet.content.text}`);
 
     const $footer = $('<footer>');
     const $time = $('<div>');
     const $icons = $('<div>').append($(`<i class="fas fa-flag"></i>`), $(`<i class="fas fa-retweet"></i>`), $(`<i class="fas fa-thumbs-up"></i>`));
-    const formattedTime = timeago.format(tweetData.created_at);
+    const formattedTime = timeago.format(tweet.created_at);
     $time.text(`${formattedTime}`);
     $footer.append($time, $icons);
 
     $tweet.append($header, $content, $footer);
     return $tweet;
   };
-  
-  const $tweet = createTweetElement(tweetData);
-  $('#tweet-container').prepend($tweet);
+
+  renderTweets(data);
 });
