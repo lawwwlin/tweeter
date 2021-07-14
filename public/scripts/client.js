06b5fa17febc6f4bb53b2da136f2283f5329f546
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-07-14 15:22:25 -0700

    add loadTweets to get data from server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,32 +4,16 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 $(document).ready(() => {
+
+  const loadTweets = () => {
+    $.getJSON('/tweets', (tweets) => {
+      console.log(tweets);
+      renderTweets(tweets);
+    });
+  };
+
+  loadTweets();
 
   const renderTweets = (tweets) => {
     $('#tweet-container').empty();
@@ -65,12 +49,18 @@ $(document).ready(() => {
   const $form = $('.new-tweet form');
   $form.submit(function(event) {
     event.preventDefault();
+    const $input = $('#tweet-text');
+    const text = $input.val();
+    if (text === null) {
+      alert('Nothing to tweet!');
+    } else if (text.length > 140) {
+      alert('Tweet is too long!');
+    }
     const urlEncodedData = $(this).serialize();
-    console.log(urlEncodedData);
     $.post('/tweets', urlEncodedData, (response) => {
       console.log(response);
+      loadTweets();
     });
   });
 
-  renderTweets(data);
 });
